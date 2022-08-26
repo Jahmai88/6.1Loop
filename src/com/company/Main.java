fe67373e60dec290ebd5d338381a839a08b41720
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 22:14:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 5; i >= 0; i--) {
+            System.out.print(i +"Hello");
+        }//end for
+        
+
+    }
+}
